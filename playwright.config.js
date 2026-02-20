0fb79ceb79e3954f27056639e72be99d98536b7f
lumpyto <lumpyto@yahoo.com>
2026-02-20 12:20:25 +0200

    small fixes

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'line',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     actionTimeout: 10000,
@@ -44,10 +44,11 @@ export default defineConfig({
     {
       name: 'chromium',
       use: {
-    viewport: null,
-    deviceScaleFactor: undefined,
+        channel: 'chrome',
+        viewport: null,
+        deviceScaleFactor: undefined,
     
-    launchOptions: {
+      launchOptions: {
       args: ['--start-maximized']
         },
       },
--- tests/webTesting.spec.js
+++ tests/webTesting.spec.js
@@ -1,25 +1,41 @@
-const { test, expect } = require('@playwright/test');
+import {test, expect} from '@playwright/test';
 import users from '../data/users.json';
 
 test.describe('JACK Entertainment Page Tests', () => {
   const baseUrl = 'https://jackentertainment.lv-stg.gameaccount.com/';
 
+  test.describe.configure({ mode: 'serial' });
+
+  let shared = {
+    username: '',
+    email:    '',
+    password: ''
+  };
+
+  let faker;
+
   test.beforeEach(async ({ page }) => {
     await page.goto(baseUrl);
+
+    if (!faker) {
+      const fakerModule = await import("@faker-js/faker");
+      faker = fakerModule.faker;
+    }
+    if (!shared.username) {
+      shared.username = faker.person.firstName().toLowerCase();
+      shared.email    = faker.internet.email();
+      shared.password = faker.internet.password({ length: 12 }) + '6';
+    }
   });
 
 test('Successful Registration', async ({ page }) => {
     await expect(page).toHaveTitle('JACK Entertainment Online | Free Casino Games & Slots');
 
-  let shortTime = new Date().toTimeString().slice(0, 5).replace(':', '');
-  let username = `testAuto${shortTime}`;
-  let email = users.newUserUser.username;
-  let password = users.newUserUser.password;
-
   await page.click('text=Sign Up');
-  await page.fill('input[name="username"]', username);
-  await page.fill('input[name="email"]', email);
-  await page.fill('input[name="password"]', password);
+  await page.fill('input[name="username"]', shared.username);
+  await page.fill('input[name="email"]', shared.email);
+  await page.fill('input[name="password"]', shared.password);
+  await page.getByRole('button', { name: 'Close' }).click();
   await page.click('button[data-qa="signup_submit_btn"]');
 
   let i = 0;
@@ -33,35 +49,42 @@ test('Successful Registration', async ({ page }) => {
   await page.locator('[data-qa="signup_checkbox"]').click();
   await page.getByRole('button', { name: 'Continue' }).click({ force: true });
 
-  console.log(`Your username is ${username}`);
-  console.log(`Your email is ${email}`);
-  console.log(`Your password is ${password}`);
+  console.log(`Your username is ${shared.username}`);
+  console.log(`Your email is ${shared.email}`);
+  console.log(`Your password is ${shared.password}`);
+
+  //Check for successful registration. Login with Username
+  await page.locator('.WelcomeBonus_iconClose__C5oab').click();
+  await page.locator('[data-qa="expand_account_menu"]', {delay:200}).click();
+  await expect(page.locator('.account-username')).toHaveText(shared.username);
+  await page.getByTitle("Logout").click();
+  await expect(page).toHaveURL("https://jackentertainment.lv-stg.gameaccount.com/");
 });
 
 
   test('Successful Login', async ({ page }) => {
-     await expect(page).toHaveTitle('JACK Entertainment Online | Free Casino Games & Slots');
+    await expect(page).toHaveTitle('JACK Entertainment Online | Free Casino Games & Slots');
     
-    let username = users.myUser.username
-    let email = users.myUser.email;
-    let password = users.myUser.password;
+    let username = shared.username
+    let email = shared.email;
+    let password = shared.password;
 
     await page.click('text=Login');
-    await page.fill('input[name="email"]', email);
+    await page.fill('input[name="email"]', username);
     await page.fill('input[name="password"]', password);
     await page.locator('button[data-qa="login_submit_btn"]').click();
 
     await page.locator('[data-qa="expand_account_menu"]').click();
-    await expect(page.locator('.account-username')).toHaveText(username);
+    await expect(page.locator('.account-username')).toHaveText(shared.username);
   });
 
 
-  test.only('Bonus History (Optional)', async ({ page }) => {
+test('Bonus History (Optional)', async ({ page }) => {
     await expect(page).toHaveTitle('JACK Entertainment Online | Free Casino Games & Slots');
     
-    let username = users.myUser.username
-    let email = users.myUser.email;
-    let password = users.myUser.password;
+    let username = shared.username
+    let email = shared.email;
+    let password = shared.password;
 
     await page.click('text=Login');
     await page.fill('input[name="email"]', email);
